7da3ceff8c1d002b04690c9607cf46b75a93c286
ttjoe <ttjoe0@gmail.com>
2023-05-29 18:39:45 +0000

    UPDATE: Add comment to routes handlers

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//Routes handlers
 app.use("/", indexRouter);
 app.use("/project", projectRouter);
 app.use("/about-me", aboutmeRouter);
