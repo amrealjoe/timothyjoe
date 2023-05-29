8189ace514c219f0b08161302c1a5a78273778ef
ttjoe <ttjoe0@gmail.com>
2023-05-29 19:11:36 +0000

    Clean all file for new node.js app

--- routes/project.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/** GET project page */
-router.get("project", (req, res, next) => {
-  res.render("project", { siteTitle: "Projects" });
-});
-
-module.exports = router;
