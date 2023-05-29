7f6b0add0e9c1a927b7c62c76dc10ca1bf33f63f
ttjoe <ttjoe0@gmail.com>
2023-05-29 20:26:52 +0000

    install dependencies

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // ./index.js
-const express = require("express");
+const app = require("express")();
+const { v4 } = require("uuid");
 
-const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-app.get("/", (req, res) => {
-  res.send("Hello, Vercel!");
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
 
 app.listen(port, () => {
