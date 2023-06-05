30c27795e6cda98212187dcf3ce5c8ba52fb7ee2
ttjoe <ttjoe0@gmail.com>
2023-06-05 18:44:16 +0000

    Create navigation component and remove includes folder

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home/', { title: 'Timothy T. Joe Portfolio' });
+  res.render('Home', { title: 'Timothy T. Joe Portfolio' });
 });
 
 module.exports = router;
