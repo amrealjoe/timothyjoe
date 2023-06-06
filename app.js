e5e2b22d72336837f480d16202216b6e6a8b1798
ttjoe <ttjoe0@gmail.com>
2023-06-06 18:22:13 +0000

    Added content to profile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var homeRouter = require('./routes/home');
-var blogRouter = require('./routes/blog');
+var profileRouter = require('./routes/profile');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //router handler middleware
 app.use('/', homeRouter);
-app.use('/blog', blogRouter);
+app.use('/profile', profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('blog/', { title: 'Blog Articles' });
+  res.render('profile/', { title: 'Blog Articles' });
 });
 
 module.exports = router;
