f7f14334b72bccc22ab5b65bc9063c13888d6852
ttjoe <ttjoe0@gmail.com>
2023-06-09 11:51:02 +0000

    Moved pages file into the view directory

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('blog/index', { title: 'Blog Articles' });
+router.get("/", function (req, res, next) {
+  res.render("Blog", { title: "Blog Articles" });
 });
 
 module.exports = router;
