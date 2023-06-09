354c45bae70839e9118c539d6c8716b024a22f1a
ttjoe <ttjoe0@gmail.com>
2023-06-09 11:35:44 +0000

    Fixed failed to lookup view

--- routes/blog.js
+++ routes/blog.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('blog/', { title: 'Blog Articles' });
+  res.render('blog/index', { title: 'Blog Articles' });
 });
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('profile/', { title: 'About Timothy T. Joe, Software Developer' });
+  res.render('profile/index', { title: 'About Timothy T. Joe, Software Developer' });
 });
 
 module.exports = router;
